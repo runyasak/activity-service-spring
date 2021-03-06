eccfd64ee496af96e1463d6d39ba23a491084aa4
Runyasak Chaengnaimuang <world.jadi@gmail.com>
2021-03-06 11:35:54 +0700

    feat: implement lombok Getter & Setter

--- src/main/java/com/example/activityservice/model/Activity.java
+++ src/main/java/com/example/activityservice/model/Activity.java
@@ -1,31 +1,12 @@
 package com.example.activityservice.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class Activity {
     private Integer id;
     private String name;
     private String description;
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
 }
