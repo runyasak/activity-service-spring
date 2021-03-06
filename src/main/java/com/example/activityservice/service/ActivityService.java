09ff1bc9ef5c55437b43bed26bee255944fa9ab4
Runyasak Chaengnaimuang <world.jadi@gmail.com>
2021-03-06 14:56:20 +0700

    feat: add Example pattern

--- src/main/java/com/example/activityservice/service/ActivityService.java
+++ src/main/java/com/example/activityservice/service/ActivityService.java
@@ -2,6 +2,7 @@ package com.example.activityservice.service;
 
 import com.example.activityservice.model.Activity;
 import com.example.activityservice.repository.ActivityRepository;
+import org.springframework.data.domain.Example;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -20,6 +21,12 @@ public class ActivityService {
     }
 
     public List<Activity> getByUser(String username) {
+        Activity activity = Activity.builder().createdBy(username).build();
+
+        // 3: Example
+        // Example<Activity> example = Example.of(activity);
+        // return activityRepository.findAll(example);
+        
         return activityRepository.findAllByCreatedBy(username);
     }
 }
