15fbffca5b512285178a9943d190590f5db3c01c
Runyasak Chaengnaimuang <world.jadi@gmail.com>
2021-03-06 14:31:37 +0700

    spec: update getAllTest

--- src/test/java/com/example/activityservice/service/ActivityServiceTest.java
+++ src/test/java/com/example/activityservice/service/ActivityServiceTest.java
@@ -4,15 +4,32 @@ import com.example.activityservice.model.Activity;
 import com.example.activityservice.repository.ActivityRepository;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 
 import java.util.List;
 
 class ActivityServiceTest {
 
+    @Mock
+    ActivityRepository activityRepository;
+
+    @InjectMocks
+    ActivityService activityService;
+
     @Test
     void getAllTest() {
-        ActivityRepository activityRepository;
-        ActivityService activityService = new ActivityService(activityRepository);
+        Activity activity1 = new Activity();
+        activity1.setName("Logout");
+        activity1.setDescription("World is logged out");
+
+        Activity activity2 = new Activity();
+
+        List<Activity> activities = List.of(activity1, activity2);
+
+        Mockito.when(activityRepository.findAll()).thenReturn(activities);
+
         List<Activity> actual = activityService.getAll();
 
         Assertions.assertEquals(2, actual.size());
