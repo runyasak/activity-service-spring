18ba07aae1fb3183d8c9743ee8f5247f2e7333ed
Runyasak Chaengnaimuang <world.jadi@gmail.com>
2021-03-06 11:29:39 +0700

    feat: add UserController and implement

--- src/main/java/com/example/activityservice/ActivityServiceApplication.java
+++ src/main/java/com/example/activityservice/ActivityServiceApplication.java
@@ -2,8 +2,10 @@ package com.example.activityservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.example.activityservice", "com.example.utils"})
 public class ActivityServiceApplication {
 
 	public static void main(String[] args) {
