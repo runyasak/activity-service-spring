4337b0322e1f184a0768ffcf4fe272b352601040
Runyasak Chaengnaimuang <world.jadi@gmail.com>
2021-03-06 11:32:12 +0700

    feat: implement RestController

--- src/main/java/com/example/activityservice/controller/ActivityController.java
+++ src/main/java/com/example/activityservice/controller/ActivityController.java
@@ -1,16 +1,14 @@
 package com.example.activityservice.controller;
 
 import com.example.activityservice.model.Activity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class ActivityController {
 
     @RequestMapping(value = "/activity", method = RequestMethod.GET)
-    @ResponseBody
     public Activity getActivity() {
         Activity activity = new Activity();
         activity.setId(1);
